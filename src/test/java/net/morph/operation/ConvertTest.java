0a5b5987cf931e3ed0b62706a891afece1108829
mbenson <gudnabrsam@gmail.com>
2012-07-25 15:34:42 -0500

    rename; more tests

---
+++ src/test/java/net/morph/operation/ConvertTest.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package net.morph.operation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import net.morph.Morph;
+import net.morph.MorphContext;
+import net.morph.position.Constant;
+
+import org.junit.Before;
+import org.junit.Test;
+
+@SuppressWarnings("deprecation")
+public class ConvertTest {
+    public enum MetasyntacticVariable {
+        FOO, BAR, BAZ;
+    }
+
+    private MorphContext morphContext;
+
+    @Before
+    public void setup() {
+        morphContext = Morph.standard().context();
+    }
+
+    @Test
+    public void testImmutableAssignable() {
+        assertNull(morphContext.perform(new Convert<String, CharSequence>(new Constant<String>(null) {
+        }, CharSequence.class)));
+        assertEquals("", morphContext.perform(new Convert<String, CharSequence>(Constant.of(""), CharSequence.class)));
+        assertEquals("", morphContext.perform(new Convert<String, String>(Constant.of(""), String.class)));
+        assertSame(MetasyntacticVariable.FOO,
+            morphContext.perform(new Convert<MetasyntacticVariable, MetasyntacticVariable>(Constant
+                .of(MetasyntacticVariable.FOO), MetasyntacticVariable.class)));
+        assertSame(MetasyntacticVariable.FOO, morphContext.perform(new Convert<MetasyntacticVariable, Enum<?>>(Constant
+            .of(MetasyntacticVariable.FOO), Enum.class)));
+        assertEquals(Integer.valueOf(666),
+            morphContext.perform(new Convert<Integer, Integer>(Constant.of(Integer.valueOf(666)), Integer.class)));
+        assertEquals(Integer.valueOf(666),
+            morphContext.perform(new Convert<Integer, Number>(Constant.of(Integer.valueOf(666)), Number.class)));
+    }
+
+    @Test
+    public void testCoerciontoString() {
+        assertEquals("666",
+            morphContext.perform(new Convert<Integer, String>(Constant.of(Integer.valueOf(666)), String.class)));
+    }
+
+    @Test
+    public void testCoerciontoEnum() {
+        assertNull(morphContext.perform(new Convert<Object, MetasyntacticVariable>(new Constant<Object>(null) {
+        }, MetasyntacticVariable.class)));
+        assertNull(morphContext.perform(new Convert<String, MetasyntacticVariable>(Constant.of(""),
+            MetasyntacticVariable.class)));
+        assertSame(MetasyntacticVariable.FOO, morphContext.perform(new Convert<String, MetasyntacticVariable>(Constant
+            .of("FOO"), MetasyntacticVariable.class)));
+    }
+
+    @Test
+    public void testCoercionToBoolean() {
+        assertFalse(morphContext.perform(new Convert<Object, Boolean>(new Constant<Object>(null) {
+        }, Boolean.class)).booleanValue());
+        assertFalse(morphContext.perform(new Convert<String, Boolean>(Constant.of(""), Boolean.class)).booleanValue());
+        assertFalse(morphContext.perform(new Convert<String, Boolean>(Constant.of("false"), Boolean.class))
+            .booleanValue());
+        assertFalse(morphContext.perform(new Convert<String, Boolean>(Constant.of("whatever"), Boolean.class))
+            .booleanValue());
+        assertTrue(morphContext.perform(new Convert<String, Boolean>(Constant.of("true"), Boolean.class))
+            .booleanValue());
+    }
+
+}
