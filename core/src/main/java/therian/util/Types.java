e17962b6c598a66c17e26f300c9c6f97e2cae93c
Matt Benson <gudnabrsam@gmail.com>
2013-04-05 18:00:50 -0500

    utility

--- core/src/main/java/therian/util/Types.java
+++ core/src/main/java/therian/util/Types.java
@@ -2,6 +2,7 @@ package therian.util;
 
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
+import java.util.Map;
 
 import org.apache.commons.lang3.reflect.TypeUtils;
 
@@ -12,5 +13,19 @@ public class Types {
             return TypeUtils.normalizeUpperBounds(((TypeVariable<?>) type).getBounds())[0];
         }
         return type;
+    }
+
+    /**
+     * Get the argument to a given type variable, unrolling variable-to-variable assignments among a class hierarchy
+     * @param typeArguments as from {@link TypeUtils#getTypeArguments(Type, Class)}
+     * @param typeVariable
+     * @return assigned Type
+     */
+    public static Type get(Map<TypeVariable<?>, Type> typeArguments, TypeVariable<?> typeVariable) {
+        Type result = typeArguments.get(typeVariable);
+        while (result instanceof TypeVariable<?>) {
+            result = typeArguments.get(result);
+        }
+        return result;
     }
 }
