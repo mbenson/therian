41709258ca32abbcf70d1d9f7e34c088920f3df5
Matt Benson <gudnabrsam@gmail.com>
2013-05-06 18:04:35 -0500

    DefaultToEnumerationConverter

---
+++ core/src/main/java/therian/operator/convert/DefaultToEnumerationConverter.java
@@ -0,0 +1,75 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package therian.operator.convert;
+
+import java.lang.reflect.Type;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import org.apache.commons.lang3.reflect.TypeUtils;
+
+import therian.Operator.DependsOn;
+import therian.TherianContext;
+import therian.buildweaver.StandardOperator;
+import therian.operation.Convert;
+import therian.operation.GetElementType;
+import therian.operator.getelementtype.GetEnumerationElementType;
+import therian.position.Position;
+import therian.util.Positions;
+import therian.util.Types;
+
+/**
+ * Attempts to convert to {@link Iterator} and thence to {@link Enumeration}.
+ */
+@SuppressWarnings("rawtypes")
+@StandardOperator
+@DependsOn({ DefaultToIteratorConverter.class, GetEnumerationElementType.class, IteratorToEnumeration.class })
+public class DefaultToEnumerationConverter extends Converter<Object, Enumeration> {
+
+    @Override
+    public boolean perform(TherianContext context, Convert<? extends Object, ? super Enumeration> convert) {
+        final Type targetElementType = context.eval(GetElementType.of(convert.getTargetPosition()));
+        final Position.ReadWrite<Iterator<?>> iterator =
+            Positions.readWrite(Types.parameterize(Iterator.class, targetElementType));
+        return context.evalSuccess(Convert.to(iterator, convert.getSourcePosition()))
+            && context.forwardTo(Convert.to(convert.getTargetPosition(), iterator));
+    }
+
+    @Override
+    public boolean supports(TherianContext context, Convert<?, ? super Enumeration> convert) {
+        if (!super.supports(context, convert)) {
+            return false;
+        }
+        final Convert<?, Iterator> toIterator = Convert.to(Iterator.class, convert.getSourcePosition());
+
+        if (!context.supports(toIterator)) {
+            return false;
+        }
+        final GetElementType<?> getTargetElementType = GetElementType.of(convert.getTargetPosition());
+        if (!context.supports(getTargetElementType)) {
+            return false;
+        }
+        final GetElementType<?> getSourceElementType = GetElementType.of(convert.getSourcePosition());
+        if (!context.supports(getSourceElementType)) {
+            return false;
+        }
+        final Type targetElementType = context.eval(getTargetElementType);
+        final Type sourceElementType = context.eval(getSourceElementType);
+
+        return TypeUtils.isAssignable(sourceElementType, targetElementType);
+    }
+
+}
---
+++ core/src/test/java/therian/operator/convert/DefaultToEnumerationConverterTest.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package therian.operator.convert;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Enumeration;
+
+import org.junit.Test;
+
+import therian.TherianModule;
+import therian.TypeLiteral;
+import therian.operation.Convert;
+import therian.operator.OperatorTest;
+import therian.testfixture.MetasyntacticVariable;
+import therian.util.Positions;
+
+/**
+ *
+ */
+public class DefaultToEnumerationConverterTest extends OperatorTest {
+
+    @Override
+    protected TherianModule module() {
+        return TherianModule.create().withOperators(new DefaultToEnumerationConverter());
+    }
+
+    @Test
+    public void testSingleton() {
+        final Enumeration<MetasyntacticVariable> result =
+            therianContext.eval(Convert.to(new TypeLiteral<Enumeration<MetasyntacticVariable>>() {},
+                Positions.readOnly(MetasyntacticVariable.FOO)));
+        assertTrue(result.hasMoreElements());
+        assertSame(MetasyntacticVariable.FOO, result.nextElement());
+        assertFalse(result.hasMoreElements());
+    }
+
+    @Test
+    public void testArray() {
+        final Enumeration<MetasyntacticVariable> result =
+            therianContext.eval(Convert.to(new TypeLiteral<Enumeration<MetasyntacticVariable>>() {},
+                Positions.readOnly(MetasyntacticVariable.values())));
+        for (MetasyntacticVariable foo : MetasyntacticVariable.values()) {
+            assertTrue(result.hasMoreElements());
+            assertSame(foo, result.nextElement());
+        }
+        assertFalse(result.hasMoreElements());
+    }
+
+    @Test
+    public void testPrimitiveArray() {
+        final int[] beast = new int[] { 6, 6, 6 };
+        final Enumeration<Integer> result =
+            therianContext.eval(Convert.to(new TypeLiteral<Enumeration<Integer>>() {}, Positions.readOnly(beast)));
+        for (int i = 0; i < beast.length; i++) {
+            assertTrue(result.hasMoreElements());
+            assertEquals(beast[i], result.nextElement().intValue());
+        }
+        assertFalse(result.hasMoreElements());
+    }
+}
