aa8d1cf0e61829f9feb27e3df7eec39c8afb2302
Matt Benson <gudnabrsam@gmail.com>
2013-05-08 16:39:44 -0500

    format; deps; s/IterableCopier/ContainerCopier/

--- core/src/main/java/therian/operator/copy/ElementCopier.java
+++ core/src/main/java/therian/operator/copy/ElementCopier.java
@@ -6,18 +6,23 @@ import org.apache.commons.lang3.reflect.TypeUtils;
 
 import therian.Operator;
 import therian.TherianContext;
+import therian.Operator.DependsOn;
 import therian.buildweaver.StandardOperator;
 import therian.operation.Copy;
 import therian.operation.Size;
+import therian.operator.size.DefaultSizeOperator;
+import therian.operator.size.SizeOfCollection;
+import therian.operator.size.SizeOfIterable;
 import therian.position.Position;
 import therian.position.Position.ReadWrite;
 import therian.position.relative.Element;
 
 /**
  * Tries to copy between arrays/iterables using {@link Element} positions. This should be more efficient than
- * {@link IterableCopier} where usable. Where elements must be added, only {@link List} targets are supported.
+ * {@link ContainerCopier} where usable. Where elements must be added, only {@link List} targets are supported.
  */
 @StandardOperator
+@DependsOn({ DefaultSizeOperator.class, SizeOfCollection.class, SizeOfIterable.class })
 public class ElementCopier implements Operator<Copy<?, ?>> {
     private interface ElementFactory {
         ReadWrite<?> element(int index);
@@ -29,7 +34,7 @@ public class ElementCopier implements Operator<Copy<?, ?>> {
         final ElementFactory targetElementFactory = createElementFactory(copy.getTargetPosition());
         for (int i = 0, sz = context.eval(Size.of(copy.getSourcePosition())); i < sz; i++) {
             if (context
-                    .evalSuccessIfSupported(Copy.to(targetElementFactory.element(i), sourceElementFactory.element(i)))) {
+                .evalSuccessIfSupported(Copy.to(targetElementFactory.element(i), sourceElementFactory.element(i)))) {
                 continue;
             }
             return false;
@@ -43,7 +48,7 @@ public class ElementCopier implements Operator<Copy<?, ?>> {
         final int targetSize = context.eval(Size.of(copy.getTargetPosition()));
 
         return sourceSize <= targetSize && createElementFactory(copy.getSourcePosition()) != null
-                && createElementFactory(copy.getTargetPosition()) != null;
+            && createElementFactory(copy.getTargetPosition()) != null;
     }
 
     private static ElementFactory createElementFactory(final Position.Readable<?> source) {
