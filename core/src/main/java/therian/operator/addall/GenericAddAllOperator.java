40af2c95d5591121b4d177fa219d8ec5b570a0c4
Matt Benson <gudnabrsam@gmail.com>
2013-05-08 16:34:00 -0500

    get GenericAddAllOperator working

--- core/src/main/java/therian/operator/addall/GenericAddAllOperator.java
+++ core/src/main/java/therian/operator/addall/GenericAddAllOperator.java
@@ -2,47 +2,75 @@ package therian.operator.addall;
 
 import java.lang.reflect.Type;
 
+import org.apache.commons.lang3.reflect.TypeUtils;
+
 import therian.Operator;
+import therian.Operator.DependsOn;
 import therian.TherianContext;
 import therian.buildweaver.StandardOperator;
 import therian.operation.Add;
 import therian.operation.AddAll;
 import therian.operation.Convert;
 import therian.operation.GetElementType;
+import therian.operator.add.AddToCollection;
+import therian.operator.add.AddToListIterator;
+import therian.operator.convert.DefaultToListConverter;
+import therian.operator.convert.EnumerationToList;
+import therian.operator.convert.IterableToList;
+import therian.position.Position;
 import therian.util.Positions;
 
+/**
+ * Attempts to convert source to {@link Iterable}, then {@link Add} each element to target. Expressly rejects array
+ * targets in favor of {@link AddAllToArray}, which is more efficient for this job.
+ */
 @StandardOperator
+@DependsOn({ AddToCollection.class, AddToListIterator.class, IterableToList.class, EnumerationToList.class,
+    DefaultToListConverter.class })
 public class GenericAddAllOperator implements Operator<AddAll<?, ?>> {
 
     @Override
-    public boolean perform(TherianContext context, AddAll<?, ?> operation) {
-        final Type sourceElementType =
-            context.evalIfSupported(GetElementType.of(operation.getSourcePosition()), operation.getSourcePosition()
-                .getType());
+    public boolean perform(TherianContext context, AddAll<?, ?> addAll) {
+        final Type sourceElementType = context.eval(GetElementType.of(addAll.getSourcePosition()));
 
-        for (Object o : context.eval(Convert.to(Iterable.class, operation.getSourcePosition()))) {
-            if (!context.evalSuccess(Add.to(operation.getTargetPosition(), Positions.readOnly(sourceElementType, o)))) {
+        final Position.ReadWrite<Object> sourceElement = Positions.readWrite(sourceElementType);
+
+        boolean result = false;
+
+        for (Object o : context.eval(Convert.to(Iterable.class, addAll.getSourcePosition()))) {
+            sourceElement.setValue(o);
+            final Add<Object, ?> add = Add.to(addAll.getTargetPosition(), sourceElement);
+            result = context.eval(add).booleanValue() || result;
+            if (!add.isSuccessful()) {
                 return false;
             }
         }
+        addAll.setResult(result);
         return true;
     }
 
     @Override
-    public boolean supports(TherianContext context, AddAll<?, ?> operation) {
-        final Type sourceElementType =
-            context.evalIfSupported(GetElementType.of(operation.getSourcePosition()), operation.getSourcePosition()
-                .getType());
-
+    public boolean supports(TherianContext context, AddAll<?, ?> addAll) {
+        if (TypeUtils.isArrayType(addAll.getTargetPosition().getType())) {
+            return false;
+        }
+        final GetElementType<?> getSourceElementType = GetElementType.of(addAll.getSourcePosition());
+        if (!context.supports(getSourceElementType)) {
+            return false;
+        }
         @SuppressWarnings("rawtypes")
-        final Convert<?, Iterable> toIterable = Convert.to(Iterable.class, operation.getSourcePosition());
+        final Convert<?, Iterable> toIterable = Convert.to(Iterable.class, addAll.getSourcePosition());
 
         if (!context.supports(toIterable)) {
             return false;
         }
 
+        final Type sourceElementType = context.eval(getSourceElementType);
+        final Position.ReadWrite<Object> sourceElement = Positions.readWrite(sourceElementType);
+
         for (Object o : context.eval(toIterable)) {
-            if (!context.supports(Add.to(operation.getTargetPosition(), Positions.readOnly(sourceElementType, o)))) {
+            sourceElement.setValue(o);
+            if (!context.supports(Add.to(addAll.getTargetPosition(), sourceElement))) {
                 return false;
             }
         }
---
+++ core/src/test/java/therian/operator/addall/GenericAddAllOperatorTest.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package therian.operator.addall;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
+import org.junit.Test;
+
+import therian.TherianModule;
+import therian.TypeLiteral;
+import therian.operation.AddAll;
+import therian.operator.OperatorTest;
+import therian.position.Position;
+import therian.testfixture.MetasyntacticVariable;
+import therian.util.Positions;
+
+public class GenericAddAllOperatorTest extends OperatorTest {
+
+    @Override
+    protected TherianModule module() {
+        return TherianModule.create().withOperators(new GenericAddAllOperator());
+    }
+
+    @Test
+    public void testAddObjectToList() {
+        final List<String> targetList = new ArrayList<String>();
+        final Position.Readable<List<String>> target =
+            Positions.readOnly(new TypeLiteral<List<String>>() {}, targetList);
+
+        assertTrue(therianContext.eval(AddAll.to(target, Positions.readOnly("foo"))).booleanValue());
+
+        assertEquals(1, targetList.size());
+        assertEquals("foo", targetList.get(0));
+    }
+
+    @Test
+    public void testAddObjectToListIterator() {
+        final List<String> targetList = new ArrayList<String>();
+        final Position.Readable<ListIterator<String>> target =
+            Positions.readOnly(new TypeLiteral<ListIterator<String>>() {}, targetList.listIterator());
+
+        assertTrue(therianContext.eval(AddAll.to(target, Positions.readOnly("foo"))).booleanValue());
+
+        assertEquals(1, targetList.size());
+        assertEquals("foo", targetList.get(0));
+    }
+
+    @Test
+    public void testReaddObjectToSet() {
+        final Set<MetasyntacticVariable> targetSet =
+            new LinkedHashSet<MetasyntacticVariable>(Arrays.asList(MetasyntacticVariable.values()));
+        final Position.Readable<Set<MetasyntacticVariable>> target =
+            Positions.readOnly(new TypeLiteral<Set<MetasyntacticVariable>>() {}, targetSet);
+
+        assertFalse(therianContext.eval(AddAll.to(target, Positions.readOnly(MetasyntacticVariable.FOO)))
+            .booleanValue());
+
+        assertArrayEquals(MetasyntacticVariable.values(), targetSet.toArray());
+    }
+
+    @Test
+    public void testMergeIntoSet() {
+        final MetasyntacticVariable[] mv = MetasyntacticVariable.values();
+        final MetasyntacticVariable[] sourceArray = new MetasyntacticVariable[mv.length * 2];
+        System.arraycopy(mv, 0, sourceArray, 0, mv.length);
+        System.arraycopy(mv, 0, sourceArray, mv.length, mv.length);
+
+        final Set<MetasyntacticVariable> targetSet = new LinkedHashSet<MetasyntacticVariable>();
+        final Position.Readable<Set<MetasyntacticVariable>> target =
+            Positions.readOnly(new TypeLiteral<Set<MetasyntacticVariable>>() {}, targetSet);
+
+        assertTrue(therianContext.eval(AddAll.to(target, Positions.readOnly(sourceArray))));
+
+        assertArrayEquals(mv, targetSet.toArray());
+    }
+}
