51da32ca607e19c92fc78b5258c787d31998ff0c
Matt Benson <gudnabrsam@gmail.com>
2013-05-23 17:55:14 -0500

    MapToBeanCopier

---
+++ core/src/main/java/therian/operator/copy/MapToBeanCopier.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package therian.operator.copy;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.reflect.TypeUtils;
+
+import therian.Operator.DependsOn;
+import therian.TherianContext;
+import therian.buildweaver.StandardOperator;
+import therian.operation.Convert;
+import therian.operation.Copy;
+import therian.operator.convert.ELCoercionConverter;
+import therian.operator.convert.NOPConverter;
+import therian.position.Position;
+import therian.position.relative.Keyed;
+import therian.position.relative.Property;
+import therian.util.BeanProperties;
+import therian.util.BeanProperties.ReturnProperties;
+import therian.util.Positions;
+import therian.util.Types;
+
+/**
+ * Copy a {@link Map}'s properties onto a bean.
+ */
+@SuppressWarnings("rawtypes")
+@StandardOperator
+@DependsOn({ NOPConverter.class, ConvertingCopier.class, ELCoercionConverter.class })
+public class MapToBeanCopier extends Copier<Map, Object> {
+    public static final String[] IGNORED_PROPERTIES = { "class" };
+
+    @Override
+    public boolean perform(TherianContext context, Copy<? extends Map, ? extends Object> copy) {
+        final Type sourceKeyType = getKeyType(copy.getSourcePosition());
+
+        final Set<String> propertyNames =
+            BeanProperties.getPropertyNames(ReturnProperties.WRITABLE, context, copy.getTargetPosition());
+
+        boolean result = false;
+
+        final Set<?> keys = copy.getSourcePosition().getValue().keySet();
+        for (Object key : keys) {
+            final String propertyName = context.eval(Convert.to(String.class, Positions.readOnly(sourceKeyType, key)));
+            if (propertyNames.contains(propertyName)) {
+                @SuppressWarnings("unchecked")
+                final Position.Readable<?> source = Keyed.value().at(key).of(copy.getSourcePosition());
+                final Position.Readable<?> target = Property.at(propertyName).of(copy.getTargetPosition());
+                if (context.evalSuccess(Copy.to(target, source))) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return whether at least one entry can be copied
+     */
+    @Override
+    public boolean supports(TherianContext context, Copy<? extends Map, ? extends Object> copy) {
+        if (!super.supports(context, copy)) {
+            return false;
+        }
+
+        final Type sourceKeyType = getKeyType(copy.getSourcePosition());
+
+        final Set<String> propertyNames =
+            BeanProperties.getPropertyNames(ReturnProperties.WRITABLE, context, copy.getTargetPosition());
+
+        final Set<?> keys = copy.getSourcePosition().getValue().keySet();
+        for (Object key : keys) {
+            final String propertyName = context.eval(Convert.to(String.class, Positions.readOnly(sourceKeyType, key)));
+            if (propertyNames.contains(propertyName)) {
+                @SuppressWarnings("unchecked")
+                final Position.Readable<?> source = Keyed.value().at(key).of(copy.getSourcePosition());
+                final Position.Readable<?> target = Property.at(propertyName).of(copy.getTargetPosition());
+                if (context.supports(Copy.to(target, source))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Type getKeyType(Position<? extends Map> target) {
+        return ObjectUtils.defaultIfNull(
+            Types.unrollVariables(TypeUtils.getTypeArguments(target.getType(), Map.class),
+                Map.class.getTypeParameters()[0]), Object.class);
+    }
+}
---
+++ core/src/main/java/therian/operator/copy/MapToBeanCopierTest.java
@@ -0,0 +1,148 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package therian.operator.copy;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import therian.OperationException;
+import therian.TherianModule;
+import therian.TypeLiteral;
+import therian.operation.Convert;
+import therian.operation.Copy;
+import therian.operator.OperatorTest;
+import therian.operator.convert.DefaultCopyingConverter;
+import therian.testfixture.Employee;
+import therian.testfixture.MetasyntacticVariable;
+import therian.util.Positions;
+
+/**
+ *
+ */
+public class MapToBeanCopierTest extends OperatorTest {
+    public static class StringWrapper {
+        final String value;
+
+        StringWrapper(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    @Override
+    protected TherianModule module() {
+        return TherianModule.create().withOperators(new MapToBeanCopier(), new DefaultCopyingConverter());
+    }
+
+    @Test
+    public void testCopy() {
+        final Map<String, String> sourceMap = new HashMap<String, String>();
+        sourceMap.put("firstName", "Beetle");
+        sourceMap.put("lastName", "Bailey");
+
+        final Employee targetValue = new Employee();
+        assertTrue(therianContext.evalSuccess(Copy.to(Positions.readOnly(targetValue),
+            Positions.readOnly(new TypeLiteral<Map<String, String>>() {}, sourceMap))));
+
+        assertEquals("Beetle", targetValue.getFirstName());
+        assertNull(targetValue.getMiddleName());
+        assertEquals("Bailey", targetValue.getLastName());
+    }
+
+    @Test
+    public void testCopyWithReadOnlyProperty() {
+        final Map<String, String> sourceMap = new HashMap<String, String>();
+        sourceMap.put("firstName", "Beetle");
+        sourceMap.put("middleName", "Aloysius");
+        sourceMap.put("lastName", "Bailey");
+
+        final Employee targetValue = new Employee();
+        assertTrue(therianContext.evalSuccess(Copy.to(Positions.readOnly(targetValue),
+            Positions.readOnly(new TypeLiteral<Map<String, String>>() {}, sourceMap))));
+
+        assertEquals("Beetle", targetValue.getFirstName());
+        assertNull(targetValue.getMiddleName());
+        assertEquals("Bailey", targetValue.getLastName());
+    }
+
+    @Test(expected = OperationException.class)
+    public void testCopyWithOnlyReadOnlyProperty() {
+        final Map<String, String> sourceMap = new HashMap<String, String>();
+        sourceMap.put("middleName", "Aloysius");
+
+        final Employee targetValue = new Employee();
+        therianContext.eval(Copy.to(Positions.readOnly(targetValue),
+            Positions.readOnly(new TypeLiteral<Map<String, String>>() {}, sourceMap)));
+    }
+
+    @Test
+    public void testCopyWithOnePropertyAndExtraneousValues() {
+        final Map<String, String> sourceMap = new HashMap<String, String>();
+        sourceMap.put("firstName", "Beetle");
+        for (MetasyntacticVariable mv : MetasyntacticVariable.values()) {
+            sourceMap.put(mv.name(), mv.name().toLowerCase());
+        }
+
+        final Employee targetValue = new Employee();
+        assertTrue(therianContext.evalSuccess(Copy.to(Positions.readOnly(targetValue),
+            Positions.readOnly(new TypeLiteral<Map<String, String>>() {}, sourceMap))));
+
+        assertEquals("Beetle", targetValue.getFirstName());
+        assertNull(targetValue.getMiddleName());
+        assertNull(targetValue.getLastName());
+    }
+
+    @Test
+    public void testConvert() {
+        final Map<String, Object> sourceMap = new HashMap<String, Object>();
+        sourceMap.put("firstName", "Beetle");
+        sourceMap.put("lastName", "Bailey");
+
+        final Employee result =
+            therianContext.eval(Convert.to(Employee.class,
+                Positions.readOnly(new TypeLiteral<Map<String, Object>>() {}, sourceMap)));
+
+        assertEquals("Beetle", result.getFirstName());
+        assertNull(result.getMiddleName());
+        assertEquals("Bailey", result.getLastName());
+    }
+
+    @Test
+    public void testConvertOtherKeyType() {
+        final Map<StringWrapper, StringWrapper> sourceMap = new HashMap<StringWrapper, StringWrapper>();
+        sourceMap.put(new StringWrapper("firstName"), new StringWrapper("Beetle"));
+        sourceMap.put(new StringWrapper("lastName"), new StringWrapper("Bailey"));
+
+        final Employee result =
+            therianContext.eval(Convert.to(Employee.class,
+                Positions.readOnly(new TypeLiteral<Map<StringWrapper, StringWrapper>>() {}, sourceMap)));
+
+        assertEquals("Beetle", result.getFirstName());
+        assertNull(result.getMiddleName());
+        assertEquals("Bailey", result.getLastName());
+    }
+
+}
